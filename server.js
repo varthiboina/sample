03dd4f69110b8ed4ce5b03ab20d54624bdb03ff3
dan <dhanushvarthiboina@gmail.com>
2025-05-26 17:43:07 -0400

    check14

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3004;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World !! retrotale hahaha');
 });
 
 app.get('/hello', (req, res) => {
-  res.status(200).json({message : 'Hello from Retrotale'});
+  res.status(200).json({message : 'Hello from Retrotale git'});
 });
 
 app.listen(port, () => {
--- views/thisisgitfile.js
+++ views/thisisgitfile.js
@@ -1,1 +1,1 @@
-gitfile
+gitfile !!
