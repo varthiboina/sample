b4207eb687735599686f4a7accf249805839b7ae
dan <dhanushvarthiboina@gmail.com>
2025-05-26 17:56:20 -0400

    check17

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/hello', (req, res) => {
-  res.status(200).json({message : 'Hello from Retrotale github webhook'});
+  res.status(200).json({message : 'Hello from Retrotale github webhook !!'});
 });
 
 app.listen(port, () => {
